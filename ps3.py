4c9c3ceda81a1bc16ca815e246f79fcccb15d603
andre-lie <andretw@yahoo.com>
2021-01-05 22:04:35 +1100

    PS3 P2_ER

--- ps3.py
+++ ps3.py
@@ -305,7 +305,8 @@ class EmptyRoom(RectangularRoom):
         """
         Returns: an integer; the total number of tiles in the room
         """
-        raise NotImplementedError
+        #raise NotImplementedError
+        return self.width * self.height
         
     def is_position_valid(self, pos):
         """
@@ -313,13 +314,15 @@ class EmptyRoom(RectangularRoom):
         
         Returns: True if pos is in the room, False otherwise.
         """
-        raise NotImplementedError
+        #raise NotImplementedError
+        return self.is_position_in_room(pos)
         
     def get_random_position(self):
         """
         Returns: a Position object; a valid random position (inside the room).
         """
-        raise NotImplementedError
+        #raise NotImplementedError
+        return Position(random.uniform(0,self.width), random.uniform(0,self.height))
 
 class FurnishedRoom(RectangularRoom):
     """
